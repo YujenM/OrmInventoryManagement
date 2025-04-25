ca8f24ea28da76e6f6a8008b6f9b4535203a5616
YujenM <maharjanyuzen@gmail.com>
2025-04-25 07:07:21 +0545

    Auth-token added in google login

--- server/controllers/goggleAuth/authController.js
+++ server/controllers/goggleAuth/authController.js
@@ -1,12 +1,33 @@
 require('../../config/passport');
 const passport = require('passport');
+const jwt=require('jsonwebtoken');
+
 
 const googleAuth = passport.authenticate('google-app', { scope: ['profile', 'email'] });
 
-const googleCallback = passport.authenticate('google-app', {
-  failureRedirect: '/login',
-  successRedirect: '/dashboard/googleauth'
-});
+const googleCallback = (req, res, next) => {
+  passport.authenticate('google-app', { session: false }, (err, user) => {
+    if (err || !user) {
+      return res.redirect('/login');
+    }
+
+    // Generate JWT
+    const token = jwt.sign(
+      {
+        id: user.id,
+        email: user.email,
+        role: 'user'
+      },
+      process.env.SECRET,
+      { expiresIn: '7d' }
+    );
+    return res.status(200).json({
+      message: 'Login successful',
+      token
+    });
+  })(req, res, next);
+};
+
 
 const logout = (req, res) => {
   req.logout(() => {
